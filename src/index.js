9e6ca210f5ec89b2236c5a0b7468ab22431d760b
Bartek-Szewczyk <ba20001000@gmail.com>
2022-11-06 22:31:55 +0100

    added images in post

--- src/index.js
+++ src/index.js
@@ -10,8 +10,6 @@ import Books from "./pages/books/books";
 import Files from "./pages/files/files";
 import About from "./pages/about/about";
 import { Helmet } from "react-helmet";
-import icon from "./assets/logo.png";
-
 const client = new ApolloClient({
   uri: "https://graphql.prepr.io/523851c92aa233426d454cc96c3e4485672cea26e59bf08847bdc5cf31427618",
   cache: new InMemoryCache(),
--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -4,12 +4,16 @@ import "./about.scss";
 import Loading from "../../components/loading/loading";
 import ArrowIcon from "../../assets/arrowIcon";
 import Modal from "../../components/modal/modal";
+import Lightbox from "react-spring-lightbox";
+import NextIcon from "../../assets/nextIcon";
+import PrevIcon from "../../assets/prevIcon";
+import CloseIcon from "../../assets/closeIcon";
 
 const ABOUT_QUERY = gql`
   {
     Posts {
       items {
-        imagines {
+        images {
           url
         }
         title
@@ -29,19 +33,81 @@ function About() {
   const { data, loading, error } = useQuery(ABOUT_QUERY);
   const [showModal, setShowModal] = useState(false);
   const [modalContent, setModalContent] = useState({});
+  const [photoIndex, setPhotoIndex] = useState(1);
+  const [images, setImages] = useState([]);
+  const [isOpen, setIsOpen] = useState(false);
   if (loading) return <Loading />;
   if (error) return <pre>{error.message}</pre>;
 
   const showPost = (data) => {
+    setImages(
+      data.images.map((item) => {
+        return { src: item.url };
+      })
+    );
     setModalContent(data);
     setShowModal(true);
   };
   const handleClose = () => {
     setShowModal(false);
   };
+  console.log(data);
+  const openPhoto = (index) => {
+    console.log(index);
+    setPhotoIndex(index);
+    setIsOpen(true);
+  };
+  const onClose = () => {
+    setIsOpen(false);
+  };
+
+  const gotoPrevious = () => photoIndex > 0 && setPhotoIndex(photoIndex - 1);
+
+  const gotoNext = () =>
+    photoIndex + 1 < images.length && setPhotoIndex(photoIndex + 1);
+
   return (
-    <>
+    <div>
       <Modal show={showModal} handleClose={handleClose}>
+        {/* {isOpen && ( */}
+        <Lightbox
+          isOpen={isOpen}
+          onPrev={gotoPrevious}
+          onNext={gotoNext}
+          images={images}
+          currentIndex={photoIndex}
+          onClose={() => setIsOpen(false)}
+          style={{ background: "rgba(0, 0, 0, 0.5)" }}
+          singleClickToZoom
+          renderPrevButton={({ canPrev }) => (
+            <PrevIcon
+              onClick={gotoPrevious}
+              classProp="imgIcon"
+              disabled={!canPrev}
+            />
+          )}
+          renderNextButton={({ canNext }) => (
+            <NextIcon
+              onClick={gotoNext}
+              classProp="imgIcon"
+              disabled={!canNext}
+            />
+          )}
+          renderHeader={() => (
+            <CloseIcon onClick={onClose} classProp="closeIcon" />
+          )}
+          // mainSrc={images[photoIndex]}
+          // nextSrc={images[(photoIndex + 1) % images.length]}
+          // prevSrc={images[(photoIndex + images.length - 1) % images.length]}
+          // onCloseRequest={() => setIsOpen(false)}
+          // onMovePrevRequest={() =>
+          //   setPhotoIndex((photoIndex + images.length - 1) % images.length)
+          // }
+          // onMoveNextRequest={() =>
+          //   setPhotoIndex((photoIndex + 1) % images.length)
+          // }
+        />
+        {/* )} */}
         <div className="aboutContainer__modal">
           <p className="aboutContainer__modal__title">{modalContent?.title}</p>
           <div
@@ -50,6 +116,16 @@ function About() {
               __html: modalContent?.content?.[0]?.html,
             }}
           ></div>
+          <div className="aboutContainer__modal__images">
+            {modalContent?.images?.map((item, index) => (
+              <img
+                className="aboutContainer__modal__images__image"
+                src={item.url}
+                alt=""
+                onClick={() => openPhoto(index)}
+              />
+            ))}
+          </div>
         </div>
       </Modal>
       <div className="aboutWrapper">
@@ -72,7 +148,7 @@ function About() {
           ))}
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
